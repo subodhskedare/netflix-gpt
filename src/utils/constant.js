6fa79dc9cc9a1bc5634a09fa75297d54f710e3a3
subodh.k <subodh.k@servify.in>
2025-10-28 08:49:16 +0530

    ferching movies data

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,4 +6,14 @@ const BACKGROUND_IMG =
 
 const USER_ICON_IMG =
   "https://occ-0-2890-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
-export { LOGO_URL, BACKGROUND_IMG, USER_ICON_IMG };
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer " + process.env.REACT_APP_TMDB_TOKEN",
+  },
+};
+
+export { LOGO_URL, BACKGROUND_IMG, USER_ICON_IMG, options };
